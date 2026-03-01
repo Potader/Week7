acfacd69ed3bab3557ad3ee974e93285329a3635
Potader <igarc2001@gmail.com>
2026-03-01 01:53:55 -0600

    added documentation

---
+++ README.md
@@ -0,0 +1,9 @@
+# CSC 340 Week 7 Project
+
+Name: Ian Garcia
+Date: 03/01/2026
+
+## Purpose of this Project
+
+This project dynamically displays content depending on user's local time.
+Changes image, audio file, a custom checklist, and background colour based on time of day.
--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 window.onload = checkTime;
+
+// Pulling page elements into variables for display functions to manage
+
 const siteGreeting = document.getElementById("greeting");
 const routineUl = document.getElementById("ul-routine");
 const routineImage = document.getElementById("img-routine");
 const routineAudio = document.getElementById("aud-routine");
 
-// Main dynamic site function
+//**************** FUNCTIONAL FUNCTIONS **********************//
+
+// Main dynamic site function, styles page based on time of day
 function checkTime() {
   const current = new Date();
   let hours = current.getHours();
@@ -18,22 +23,30 @@ function checkTime() {
   }
 }
 
+// Uses Date object to obtain and return time based on local user setting
 function displayTime() {
   const current = new Date();
   let now = current.toLocaleTimeString();
   return now;
 }
 
+// Creates unordered list row, used in forEach loops to display arrays on webpages as lists
+// Shamelessly stolen from my week 3 work, modified for current week
 function displayCheckList(item) {
   let newItem = document.createElement("li");
   newItem.textContent = item;
   routineUl.appendChild(newItem);
 }
 
+//**************** COSMETIC FUNCTIONS **********************//
+
 function setMorning() {
+  // Cosmetic changes to site. Changes site divs to match time theme
   siteGreeting.innerHTML = `Good morning! It is currently ${displayTime()}`;
   routineImage.src = "images/morning.jpg";
   routineAudio.src = "songs/morningSong.mp3";
+
+  // Checklist array, calls displayChecklist to display it on page
   const checkList = [
     "Finally turn off the 5 snoozed alarms",
     "Brush teeth",
@@ -42,6 +55,7 @@ function setMorning() {
     "Check news",
   ];
   checkList.forEach(displayCheckList);
+
   /*
     Originally wanted to add gradient for morning as well
     Ran into repeated bug of background colour retaining css background-color property, even when removed entirely?
@@ -52,9 +66,14 @@ function setMorning() {
 }
 
 function setAfternoon() {
+  // Cosmetic changes to site. Changes site divs to match time theme
   siteGreeting.innerHTML = `Good afternoon! It is currently ${displayTime()}`;
   routineImage.src = "images/afternoon.jpg";
   routineAudio.src = "songs/afternoonSong.mp3";
+  document.body.style.background =
+    "linear-gradient(180deg,rgba(98, 171, 56, 1) 0%, rgba(82, 172, 217, 1) 100%)";
+
+  // Checklist array, calls displayChecklist to display it on page
   const checkList = [
     "Start preparing dinner",
     "Feed cats",
@@ -63,14 +82,17 @@ function setAfternoon() {
     "Relax with video games",
   ];
   checkList.forEach(displayCheckList);
-  document.body.style.background =
-    "linear-gradient(180deg,rgba(98, 171, 56, 1) 0%, rgba(82, 172, 217, 1) 100%)";
 }
 
 function setEvening() {
+  // Cosmetic changes to site. Changes site divs to match time theme
   siteGreeting.innerHTML = `Good evening! It is currently ${displayTime()}`;
   routineImage.src = "images/evening.jpg";
   routineAudio.src = "songs/eveningSong.mp3";
+  document.body.style.background =
+    "linear-gradient(180deg,rgba(15, 70, 102, 1) 0%, rgba(53, 10, 105, 1) 100%)";
+
+  // Checklist array, calls displayChecklist to display it on page
   const checkList = [
     "Eat dinner",
     "Take medicine",
@@ -79,6 +101,4 @@ function setEvening() {
     "Put on show to sleep to",
   ];
   checkList.forEach(displayCheckList);
-  document.body.style.background =
-    "linear-gradient(180deg,rgba(15, 70, 102, 1) 0%, rgba(53, 10, 105, 1) 100%)";
 }
